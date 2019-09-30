ca6d2510bc33cf54ea06de739a9994b3d66aadeb
Tjedor <theo.aw@hotmail.com>
2019-09-30 12:43:18 +0200

    add loginView and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import AlertView from './views/alertView/AlertView';
+import LoginView from './views/loginView/LoginView';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './variables.css';
 
 const App: React.FC = () => {
-  return <AlertView />;
+  return <Router>
+    <Switch>
+      <React.Fragment>
+        <Route exact path={"/"} component={AlertView}></Route>
+        <Route path={"/login"} component={LoginView}></Route>
+      </React.Fragment>
+    </Switch>
+  </Router>;
 };
 
 export default App;
---
+++ src/views/loginView/LoginView.tsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import "./LoginView.css";
+import Axios from "axios";
+import {Redirect} from "react-router-dom";
+
+const LoginView: React.FC = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+
+
+    const onSubmit = () =>{
+        console.log("lmao");
+        setRedirect(true);
+    };
+
+    const getMoney = async () => {
+        //get feideshit
+        await Axios({
+            url: "få opp farta Anders",
+            method: "GET",
+        })
+        return <Redirect to="/"/>;
+    };
+
+    const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setUsername(e.target.value);
+        console.log(username);
+
+    };
+
+    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setPassword(e.target.value);
+        console.log(password);
+
+    };
+
+    if (redirect) {
+        return <Redirect to="/"/>
+    }
+    return (
+        
+        <div className="login-container">
+            <h1>Login</h1>
+            <form onSubmit={onSubmit}>
+                <div>
+                <input name={"username"} value={username} placeholder={"Email"} onChange={handleUsername}/>
+                </div>
+                <div>
+                <input name={"password"} value={password} placeholder={"Password"} onChange={handlePassword}/>
+                </div>
+                <button type="submit"> Log in</button>
+            </form>
+            <a href={"www.pornhub.com"}>login with feide</a>
+        </div>
+    );
+};
+
+export default LoginView;
