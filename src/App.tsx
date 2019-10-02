dd594359dedd41c953adefcb6afc16aa5e75abcb
Andreas Melzer <akrimelzer@gmail.com>
2019-10-03 01:43:18 +0200

    show alerts from backend in alertView

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import LoginView from './views/loginView/LoginView';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './variables.css';
 
-const App: React.FC = () => {
-  return <Router>
-    <Switch>
-      <React.Fragment>
-        <Route exact path={"/"} component={AlertView}></Route>
-        <Route path={"/login"} component={LoginView}></Route>
-      </React.Fragment>
-    </Switch>
-  </Router>;
+const App: React.SFC = () => {
+  return (
+    <Router>
+      <Switch>
+        <React.Fragment>
+          <Route exact path={'/'} component={AlertView}></Route>
+          <Route path={'/login'} component={LoginView}></Route>
+        </React.Fragment>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- src/views/loginView/LoginView.tsx
+++ src/views/loginView/LoginView.tsx
@@ -1,70 +1,76 @@
-import React, {useState} from 'react';
-import "./LoginView.css";
-import Axios from "axios";
+import React, { useState } from 'react';
+import './LoginView.css';
+import Axios from 'axios';
 import Header from '../../components/header/Header';
-import {Redirect} from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 
 const LoginView: React.FC = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [redirect, setRedirect] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
+  const onSubmit = () => {
+    console.log('username: ' + username);
+    console.log('password: ' + password);
+    setRedirect(true);
+  };
 
-    const onSubmit = () =>{
-        console.log("lmao");
-        setRedirect(true);
-    };
+  const getMoney = async () => {
+    //get feideshit
+    await Axios({
+      url: 'få opp farta Anders',
+      method: 'GET'
+    });
+    return <Redirect to='/' />;
+  };
 
-    const getMoney = async () => {
-        //get feideshit
-        await Axios({
-            url: "få opp farta Anders",
-            method: "GET",
-        })
-        return <Redirect to="/"/>;
-    };
+  const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+    console.log(username);
+  };
 
-    const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        setUsername(e.target.value);
-        console.log(username);
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+    console.log(password);
+  };
 
-    };
-
-    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        setPassword(e.target.value);
-        console.log(password);
-
-    };
-
-    if (redirect) {
-        return <Redirect to="/"/>
-    }
-    return (
-        <div>
-             <header>
+  if (redirect) {
+    return <Redirect to='/' />;
+  }
+  return (
+    <div>
+      <header>
         <Header></Header>
       </header>
       <div className='container'>
-        <div className="login-container">
-            <h1>Login</h1>
-            <form onSubmit={onSubmit}>
-                <div>
-                <input name={"username"} value={username} placeholder={"Email"} onChange={handleUsername}/>
-                </div>
-                <div>
-                <input name={"password"} value={password} placeholder={"Password"} onChange={handlePassword}/>
-                </div>
-                <button type="submit"> Log in</button>
-            </form>
-            <a href={"feide"}>login with feide</a>
-        </div>
+        <div className='login-container'>
+          <h1>Login</h1>
+          <form onSubmit={onSubmit}>
+            <div>
+              <input
+                name={'username'}
+                value={username}
+                placeholder={'Email'}
+                onChange={handleUsername}
+              />
+            </div>
+            <div>
+              <input
+                name={'password'}
+                value={password}
+                placeholder={'Password'}
+                onChange={handlePassword}
+              />
+            </div>
+            <button type='submit'> Log in</button>
+          </form>
+          <a href={'feide'}>login with feide</a>
         </div>
       </div>
-
-
-    );
+    </div>
+  );
 };
 
 export default LoginView;
