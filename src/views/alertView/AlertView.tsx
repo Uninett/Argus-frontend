dfd0fdcb3999938c68c9fdd9d9bf5040ff310bd0
Andreas Melzer <akrimelzer@gmail.com>
2019-10-03 01:43:18 +0200

    show alerts from backend in alertView

--- src/views/alertView/AlertView.tsx
+++ src/views/alertView/AlertView.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './AlertView.css';
+import axios from 'axios';
 import Header from '../../components/header/Header';
+
 const AlertView: React.FC = () => {
-  const items = [
-    'Andreas',
-    'Ingrid',
-    'Karoline',
-    'Theo',
-    'Anders',
-    'Alexander',
-    'Peik Ove'
-  ];
+  const [alerts, setAlerts] = useState<Object[]>([]);
+
+  useEffect(() => {
+    getAlert();
+  }, []);
+
+  const getAlert = async () => {
+    axios
+      .get('http://localhost:8000/alert/all')
+      .then(response => {
+        const alertList = fixAlert(response.data);
+        setAlerts(alertList);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const fixAlert = (e: any) => {
+    let list: any = [];
+    for (let i = 0; i < e.length; i++) {
+      list.push(e[i].fields);
+    }
+    return list;
+  };
+
+  console.log(alerts);
   return (
     <div>
       <header>
-        <Header></Header>
+        <Header />
       </header>
       <div className='container'>
         <div className='alertbox'>
-          {items.map(item => (
-            <p key={item}>{item}</p>
-          ))}
+          <ul>
+            {alerts.map((alert: any) => {
+              const s: string = JSON.stringify(alert).replace(/['"{}]+/g, '');
+              return <li key={alert.alert_id}>{s.replace(/[,]+/g, ' ')}</li>;
+            })}
+          </ul>
         </div>
       </div>
     </div>
