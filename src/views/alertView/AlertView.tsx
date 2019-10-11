a65eac5f9136dc8e24ddcd192ccb286129a3a0ff
Andreas Melzer <akrimelzer@gmail.com>
2019-10-11 13:05:29 +0200

    add react-table

--- src/views/alertView/AlertView.tsx
+++ src/views/alertView/AlertView.tsx
@@ -1,50 +1,17 @@
-import React, { useState, useEffect } from "react";
-import "./AlertView.css";
-import axios from "axios";
-import Header from "../../components/header/Header";
+import React, { useState, useEffect } from 'react';
+import './AlertView.css';
+import axios from 'axios';
+import Header from '../../components/header/Header';
+import Table from '../../components/Table';
 
 const AlertView: React.FC = () => {
-  const [alerts, setAlerts] = useState<Object[]>([]);
-
-  useEffect(() => {
-    getAlert();
-  }, []);
-
-  const getAlert = async () => {
-    axios
-      .get("http://localhost:8000/alert/all")
-      .then(response => {
-        const alertList = fixAlert(response.data);
-        setAlerts(alertList);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  const fixAlert = (e: any) => {
-    let list: any = [];
-    for (let i = 0; i < e.length; i++) {
-      list.push(e[i].fields);
-    }
-    return list;
-  };
-
-  console.log(alerts);
   return (
     <div>
       <header>
         <Header />
       </header>
-      <div className="container">
-        <div className="alertbox">
-          <ul>
-            {alerts.map((alert: any) => {
-              const s: string = JSON.stringify(alert).replace(/['"{}]+/g, "");
-              return <li key={alert.alert_id}>{s.replace(/[,]+/g, " ")}</li>;
-            })}
-          </ul>
-        </div>
+      <div>
+        <Table />
       </div>
     </div>
   );
