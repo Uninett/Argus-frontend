cb46f85f765e4f047574a802e2e1e423bed3d815
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-10-11 13:24:33 +0200

    add link buttons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,8 +5,18 @@ import aaslogo from '../../Media/img//logo/logo (4).svg'
 
 const Header: React.FC = () => {
   return (
-    <div className='header'>
+    <div className='header' >
       <img src={aaslogo} alt="" className='logo'/>
+      <a id="aboutbutton" href="/about">
+        <p>
+        About
+          </p>
+        </a>
+      <a id="loginbutton" href="/login">
+        <p>
+        Login
+          </p>
+        </a>
    </div>
   );
 }
