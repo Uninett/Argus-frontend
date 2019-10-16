277a2155995523dd455cccd93fd7b41af3d0d518
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-10-16 17:30:20 +0200

    change from state to localstorage

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Header.css";
 import { Store } from "../../store";
 import aaslogo from '../../Media/img//logo/logo_white.svg'
 
 function Userbutton() {
-  const { state } = React.useContext(Store);
-  if (state.user!='' ) {
+  if (localStorage.getItem('user') ) {
     return (<div id="userbutton" className='headerbutton' >
     <p>
-    {state.user}
+    {localStorage.getItem('user')}
       </p>
     </div>);
   }
@@ -38,32 +36,3 @@ const Header: React.FC = () => {
 };
 
 export default Header;
-=======
-import React from "react";
-import "./Header.css";
-import aaslogo from '../../Media/img//logo/logo_white.svg'
-
-const Header: React.FC = () => {
-
-  return (
-    <div className='header' >
-      <a href="/">
-      <img src={aaslogo} alt="AAS logo" className='logo'/>
-      </a>
-      <p className="user">{localStorage.getItem('user')}</p>
-      <a id="aboutbutton" className='headerbutton' href="/about">
-        <p>
-        About
-          </p>
-        </a>
-      <a id="loginbutton" className='headerbutton' href="/login">
-        <p>
-        Login
-          </p>
-        </a>
-   </div>
-  );
-};
-
-export default Header;
->>>>>>> 968b7540965343fe303912ac0487f4c91af33257
