cba7eb9942d4fb9c54c213c4ceb31e0affa4b415
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-10-16 16:41:22 +0200

    show username if logged in, login if not

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,26 +3,39 @@ import "./Header.css";
 import { Store } from "../../store";
 import aaslogo from '../../Media/img//logo/logo_white.svg'
 
+function Userbutton() {
+  const { state } = React.useContext(Store);
+  if (state.user!='' ) {
+    return (<a id="loginbutton" className='headerbutton' href="/login">
+    <p>
+    {state.user}
+      </p>
+    </a>);
+  }
+  else {
+    return (<a id="loginbutton" className='headerbutton' href="/login">
+  <p>
+  Login
+    </p>
+  </a>);}
+}
+
 const Header: React.FC = () => {
   const { state } = React.useContext(Store);
   console.log(state.user);
-
+  console.log(state);
+  
   return (
     <div className='header' >
       <a href="/">
       <img src={aaslogo} alt="AAS logo" className='logo'/>
       </a>
-      <p className="user">{state.user}</p>
       <a id="aboutbutton" className='headerbutton' href="/about">
         <p>
         About
           </p>
         </a>
-      <a id="loginbutton" className='headerbutton' href="/login">
-        <p>
-        Login
-          </p>
-        </a>
+      <Userbutton></Userbutton>
    </div>
   );
 };
