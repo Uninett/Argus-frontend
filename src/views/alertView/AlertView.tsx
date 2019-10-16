b3c187a82ceeee7e7d1ef009487fd10f1509b6fa
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-10-16 14:14:15 +0200

    :lipstick: change color scheme and alter syling of react-table

--- src/views/alertView/AlertView.tsx
+++ src/views/alertView/AlertView.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './AlertView.css';
 import axios from 'axios';
 import Header from '../../components/header/Header';
-import Table from '../../components/Table';
+import Table from '../../components/react-table/Table';
+import '../../components/react-table/table.css';
 
 const AlertView: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const AlertView: React.FC = () => {
       <header>
         <Header />
       </header>
-      <div>
+      <div className='table'>
         <Table />
       </div>
     </div>
