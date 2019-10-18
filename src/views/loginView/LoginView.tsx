9b43d5204a5c1e5920054b27502d7699fd057556
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-10-18 13:02:16 +0200

    :lipstick: Add logo to loginbox

--- src/views/loginView/LoginView.tsx
+++ src/views/loginView/LoginView.tsx
@@ -3,6 +3,7 @@ import './LoginView.css';
 import Axios from 'axios';
 import { Store } from '../../store';
 import auth from '../../auth';
+import aaslogo from '../../Media/img/logo/logo_white.svg'
 
 const LoginView: React.FC<any> = props => {
   const [username, setUsername] = useState('');
@@ -43,8 +44,9 @@ const LoginView: React.FC<any> = props => {
     <div>
       <div className='container'>
         <div className='login-container'>
-          <h1>Login</h1>
-          <form onSubmit={onSubmit}>
+      <img className='login-logo' src={aaslogo} alt="logo"/>
+          <h1 className="login-header">Login</h1>
+          <form onSubmit={onSubmit} className="login-form">
             <div>
               <input
                 name={'username'}
@@ -64,7 +66,7 @@ const LoginView: React.FC<any> = props => {
             </div>
             <button type='submit'> Log in</button>
           </form>
-          <a href={'http://localhost:8000/login/dataporten/'}>
+          <a className="login-feide" href={'http://localhost:8000/login/dataporten/'}>
             login with feide
           </a>
         </div>
