db40cd43883357fa0f7c8b055a1d10c83df70862
karolbon <karolbon@stud.ntnu.no>
2019-10-20 13:11:09 +0200

    :sparkles: Add notification profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import AlertView from './views/alertView/AlertView';
-import LoginView from './views/loginView/LoginView';
-import {Route, Switch } from 'react-router-dom';
-import { ProtectedRoute } from './protected.route';
-import './variables.css';
-import './colorscheme.css';
-
+import React from "react";
+import AlertView from "./views/alertView/AlertView";
+import LoginView from "./views/loginView/LoginView";
+import { Route, Switch } from "react-router-dom";
+import { ProtectedRoute } from "./protected.route";
+import "./variables.css";
+import "./colorscheme.css";
+import NotificationProfileView from "./views/notificationProfileView/NotificationProfileView";
 
 const App: React.SFC = () => {
   return (
     <div>
       <Switch>
-        <ProtectedRoute exact path='/' component={AlertView} />
-        <Route path='/login' component={LoginView} />
-        <Route path='*' component={() => <h1>404 not found</h1>} />
+        <ProtectedRoute exact path="/" component={AlertView} />
+        <ProtectedRoute
+          path="/notification-profile"
+          component={NotificationProfileView}
+        />
+        <Route path="/login" component={LoginView} />
+        <Route path="*" component={() => <h1>404 not found</h1>} />
       </Switch>
     </div>
   );
