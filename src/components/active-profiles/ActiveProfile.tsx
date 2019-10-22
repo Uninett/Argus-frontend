7dbe12bddb7828dd57b07fdab37e1f8a3d25ce16
karolbon <karolbon@stud.ntnu.no>
2019-10-22 10:35:22 +0200

    :twisted_rightwards_arrows: Merge fix/format-active-profile

--- src/components/active-profiles/ActiveProfile.tsx
+++ src/components/active-profiles/ActiveProfile.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import './ActiveProfile.css';
-//import data from "../scheduler/data_copy";
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import "./ActiveProfile.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import moment from "moment";
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1)
   },
   input: {
-    display: 'none'
+    display: "none"
   }
 }));
 
@@ -26,10 +25,10 @@ const ActiveProfile: React.FC = () => {
 
   const fetchProfiles = async () => {
     await axios({
-      url: 'http://localhost:8000/notificationprofile/all',
-      method: 'GET',
+      url: "http://localhost:8000/notificationprofiles/",
+      method: "GET",
       headers: {
-        Authorization: 'Token ' + localStorage.getItem('token')
+        Authorization: "Token " + localStorage.getItem("token")
       }
     }).then((response: any) => {
       const data = response.data;
@@ -43,16 +42,16 @@ const ActiveProfile: React.FC = () => {
   const serializeData = (dataFromAPI: any) => {
     const profilesList: any = [];
     for (let i = 0; i < dataFromAPI.length; i++) {
-      let profile = dataFromAPI[i].fields;
-      let startDate = moment(dataFromAPI[i].fields.interval_start)
-        .format('YYYY MM DD HH mm')
-        .split(' ')
+      let profile = dataFromAPI[i];
+      let startDate = moment(dataFromAPI[i].interval_start)
+        .format("YYYY MM DD HH mm")
+        .split(" ")
         .map(function(item) {
           return parseInt(item, 10);
         });
-      let endDate = moment(dataFromAPI[i].fields.interval_stop)
-        .format('YYYY MM DD HH mm')
-        .split(' ')
+      let endDate = moment(dataFromAPI[i].interval_stop)
+        .format("YYYY MM DD HH mm")
+        .split(" ")
         .map(function(item) {
           return parseInt(item, 10);
         });
@@ -73,13 +72,13 @@ const ActiveProfile: React.FC = () => {
     return profilesList;
   };
 
-  const weekList = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+  const weekList = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
   return (
-    <div className='profile-container'>
+    <div className="profile-container">
       {profiles.map((item: any) => {
         return (
-          <div className='item-container' key={item.id}>
+          <div className="item-container" key={item.id}>
             <h3>{item.title}</h3>
             <h4>{`${
               weekList[item.startDate.getDay()]
@@ -87,9 +86,10 @@ const ActiveProfile: React.FC = () => {
               weekList[item.endDate.getDay()]
             } ${item.endDate.getHours()}`}</h4>
             <Button
-              variant='contained'
-              color='secondary'
-              className={classes.button}>
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
               Delete
             </Button>
           </div>
