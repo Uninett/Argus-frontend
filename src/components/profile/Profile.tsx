043dc1e780c706acfeddf18bd109d19f5c4d20a6
Andreas Melzer <akrimelzer@gmail.com>
2019-11-08 12:29:36 +0100

    profiles with fetch and delete

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -5,27 +5,106 @@ import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import Dialogue from '../dialogue/Dialogue';
 import Spinner from '../spinners/Spinner';
+import axios from 'axios';
 import './Profile.css';
 
 type ProfileProps = {
-  profileNames?: { value: string; label: string }[];
-  timeslots?: { value: string; label: string }[];
+  filters: { value: string; label: string }[];
+  timeslots: { value: string; label: string }[];
+  selectedFilters: { value: string; label: string }[];
+  selectedTimeslots: { value: string; label: string };
+  active: boolean;
+  deleteProfile: any;
+  media: any;
+  mediaKey: any;
 };
 
 const Profile: React.SFC<ProfileProps> = (props: ProfileProps) => {
-  const [filterNameOption, setFilterNameOption] = useState(props.profileNames);
-  const [selectedFilterName, setSelectedFilterName] = useState(null);
+  const [filterOptions, setFilterOptions] = useState(props.filters);
+  const [selectedFilters, setSelectedFilters] = useState(props.selectedFilters);
+  const [mediaOptions, setMediaOptions] = useState([
+    { label: 'Slack', value: 'SL' },
+    { label: 'Sms', value: 'SM' },
+    { label: 'Email', value: 'EM' }
+  ]);
+  const [mediaSelected, setMediaSelected] = useState(props.media);
+  const [selectedTimeslots, setSelectedTimeslots] = useState(
+    props.selectedTimeslots
+  );
+
   const [timeOptions, setTimeOptions] = useState(props.timeslots);
   const [selectedOptions, setSelectedOptions] = useState<any>(null);
   const [loading, setLoading] = useState(false);
-  const [checkBox, setCheckBox] = useState(false);
+  const [checkBox, setCheckBox] = useState(props.active);
+
+  useEffect(() => {
+    formatMedia();
+  }, []);
+
+  const formatMedia = () => {
+    for (let i = 0; i < mediaOptions.length; i++) {
+      const item = mediaOptions[i];
+      console.log(props.media);
+      console.log(item.value);
+
+      if (item.value === props.media) {
+        setMediaSelected({ value: item.value, label: item.label });
+        console.log('Dette er mediaobjectet', {
+          value: item.value,
+          label: item.label
+        });
+      }
+    }
+  };
+
+  const postNewProfile = async () => {
+    setLoading(!loading);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    await axios({
+      url: `http://localhost:8000/notificationprofiles/${selectedTimeslots.value}`,
+      method: 'PUT',
+      headers: {
+        Authorization: 'Token ' + localStorage.getItem('token')
+      },
+      data: {
+        time_slot_group: selectedTimeslots.value,
+        filters: selectedFilters.map((f: any) => {
+          return f.value;
+        }),
+        media: ['EM'],
+        active: checkBox
+      }
+    });
+  };
 
   const handleChange = (event: any) => {
     setCheckBox(event.target.checked);
   };
+  const onChangeMedia = (e: any) => {
+    setMediaSelected(e);
+  };
 
-  const onChange = (e: any) => {
-    setSelectedOptions(e);
+  const onChangeFilters = (e: any) => {
+    setSelectedFilters(e);
+  };
+
+  const onChangeTimeslots = (e: any) => {
+    setSelectedTimeslots(e);
+  };
+
+  const handleDelete = async () => {
+    //slett fra database her:
+    await axios({
+      url: `http://localhost:8000/notificationprofiles/${selectedTimeslots.value}`,
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Token ' + localStorage.getItem('token')
+      }
+    }).then((response: any) => {
+      console.log('data er slettet');
+    });
   };
 
   const handleSave = (event: any) => {
@@ -39,26 +118,40 @@ const Profile: React.SFC<ProfileProps> = (props: ProfileProps) => {
     <div className='notification-container'>
       <div className='filtername-box'>
         <div className='filtername'>
-          <h4>Filtername:</h4>
+          <h5>Filtername:</h5>
         </div>
         <div className='filter-dropdown'>
           <Select
             isMulti
-            onChange={onChange}
+            defaultValue={selectedFilters}
+            onChange={onChangeFilters}
             name='filters'
             label='Single select'
-            options={filterNameOption}
+            options={filterOptions}
           />
         </div>
       </div>
       <div className='dropdown'>
-        <h4>Timeslots:</h4>
+        <h5>Timeslots:</h5>
         <div className='timeslot-dropdown'>
           <Select
-            isMulti
-            onChange={onChange}
+            onChange={onChangeTimeslots}
+            defaultValue={selectedTimeslots}
             name='timeslots'
             options={timeOptions}
+            className='basic-multi-select'
+            classNamePrefix='select'
+          />
+        </div>
+      </div>
+      <div className='dropdown'>
+        <h5>Media:</h5>
+        <div className='media-dropdown'>
+          <Select
+            onChange={onChangeMedia}
+            defaultValue={mediaSelected}
+            name='timeslots'
+            options={mediaOptions}
             className='basic-multi-select'
             classNamePrefix='select'
           />
@@ -85,14 +178,14 @@ const Profile: React.SFC<ProfileProps> = (props: ProfileProps) => {
               variant='contained'
               color='primary'
               size='small'
-              onClick={handleSave}
+              onClick={postNewProfile}
               startIcon={<SaveIcon />}>
               Save
             </Button>
           )}
         </div>
         <div className='button-delete'>
-          <Dialogue />
+          <Dialogue handleDelete={handleDelete} />
         </div>
       </div>
     </div>
--- src/components/profileList/ProfileList.tsx
+++ src/components/profileList/ProfileList.tsx
@@ -1,39 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import './ProfileList.css';
 import Profile from '../profile/Profile';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import axios from 'axios';
 
 const ProfileList: React.FC = () => {
-  const [profiles, setProfiles] = useState<any>([]);
-  const [timeslots, setTimeSlot] = useState([
-    { value: 'timeslot1', label: 'timeslot1' },
-    { value: 'timeslot2', label: 'timeslot2' },
-    { value: 'timeslot3', label: 'timeslot3' },
-    { value: 'timeslot4', label: 'timeslot4' },
-    { value: 'timeslot5', label: 'timeslot5' }
+  const [notificationprofiles, setNotificationprofiles] = useState([]);
+  const [filters, setFilters] = useState<any>([]);
+  const [timeslots, setTimeslots] = useState<any>([]);
+  const [mediaOptions, setMediaOptions] = useState([
+    { label: 'Slack', value: 'SL' },
+    { label: 'Sms', value: 'SM' },
+    { label: 'Email', value: 'EM' }
   ]);
 
   useEffect(() => {
-    getProfiles();
+    getNotificationprofiles();
+    getFilters();
+    getTimeslots();
   }, []);
 
-  const getProfiles = async () => {
+  //fetch all notificationprofiles
+  const getNotificationprofiles = async () => {
     await axios({
-      url: 'http://localhost:8000/notificationprofiles/filters',
+      url: 'http://localhost:8000/notificationprofiles/',
       method: 'GET',
       headers: {
         Authorization: 'Token ' + localStorage.getItem('token')
       }
     }).then((response: any) => {
-      setProfiles(formatData(response.data));
+      console.log(response.data);
+      setNotificationprofiles(response.data);
     });
   };
-  console.log('dette er profilene våre', profiles);
+  //fetch all filters
+  const getFilters = async () => {
+    await axios({
+      url: 'http://localhost:8000/notificationprofiles/filters/',
+      method: 'GET',
+      headers: {
+        Authorization: 'Token ' + localStorage.getItem('token')
+      }
+    }).then((response: any) => {
+      console.log('dette er filterene vi får:', response.data);
+      setFilters(formatData(response.data));
+    });
+  };
+
+  //fetch all timeslots
+  const getTimeslots = async () => {
+    await axios({
+      url: 'http://localhost:8000/notificationprofiles/timeslotgroups/',
+      method: 'GET',
+      headers: {
+        Authorization: 'Token ' + localStorage.getItem('token')
+      }
+    }).then((response: any) => {
+      console.log('dette er timeslots vi får:', response.data);
+      setTimeslots(formatData(response.data));
+    });
+  };
 
   const formatData = (data: any) => {
     const formattedList: any = [];
     data.map((element: any) => {
       const object: any = {
-        value: element.name.replace(' ', ''),
+        value: element.pk,
         label: element.name
       };
       formattedList.push(object);
@@ -41,14 +74,58 @@ const ProfileList: React.FC = () => {
     return formattedList;
   };
 
+  const deleteProfile = (num: number) => {
+    const prof: any = notificationprofiles;
+    console.log('Dette er orginallista', notificationprofiles);
+    prof.splice(num, 1);
+    console.log('Dette er listen uten elementet', prof);
+    setNotificationprofiles(prof);
+  };
+
+  const addNotificationprofile = () => {
+    const profiles: any = notificationprofiles;
+    const object: any = { active: false, filters: [], time_slot_group: [] };
+    profiles.push();
+  };
+  const formatMedia = (media: string) => {
+    for (let i = 0; i < mediaOptions.length; i++) {
+      const item = mediaOptions[i];
+      if (item.value === media) {
+        return {
+          value: item.value,
+          label: item.label
+        };
+      }
+    }
+  };
+
   return (
-    <div>
+    <div className='profile-container'>
       <h1>Profiler:</h1>
-      {profiles.map((profile: any, index: any ) => {
+      {notificationprofiles.map((profile: any, index: any) => {
+        const timeslot: any = {
+          value: profile.time_slot_group.pk,
+          label: profile.time_slot_group.name
+        };
         return (
-          <Profile key={index} profileNames={profiles} timeslots={timeslots} />
+          <Profile
+            key={profile.pk}
+            deleteProfile={deleteProfile}
+            filters={filters}
+            selectedFilters={formatData(profile.filters)}
+            selectedTimeslots={timeslot}
+            timeslots={timeslots}
+            active={profile.active}
+            media={formatMedia(profile.media[0])}
+            mediaKey={profile.time_slot_group.pk}
+          />
         );
       })}
+      <div className='add-button'>
+        <Fab color='primary' aria-label='add' size='large'>
+          <AddIcon />
+        </Fab>
+      </div>
     </div>
   );
 };
