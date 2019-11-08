bf6570237b1e222f6326c307691e45a3b7cf8b7d
Tjedor <theo.aw@hotmail.com>
2019-11-08 09:46:40 +0100

    header and add view

---
+++ src/views/filterBuildingView/filterBuildingView.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import Header from "../../components/header/Header";
+import "../../components/react-table/table.css";
+import FilterBuilder from "../../components/filterBuilder/filterBuilder";
+import { withRouter } from "react-router-dom";
+type PropType = {
+  history: any;
+};
+
+const FilterBuildingView: React.FC<PropType> = props => {
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      <FilterBuilder />
+    </div>
+  );
+};
+
+export default withRouter(FilterBuildingView);
