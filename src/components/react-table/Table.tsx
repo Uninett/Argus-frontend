04e28b9ece94b07902b3621b849b4b4620fa9a51
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-11-09 15:06:44 +0100

    format date and link, change color scheme

--- src/components/react-table/Table.tsx
+++ src/components/react-table/Table.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactTable from "react-table";
 import "./table.css";
 import "react-table/react-table.css";
+import moment from "moment";
 
 type AlertsProps = {
   alerts: any;
 };
 
+//function formatDate(dateString: string):string {
+//  2014-09-15T11:25:42.833000+02:00
+//}
+
 const Table: React.FC<AlertsProps> = props => {
+  
   const columns: any = [
     {
       Header: "Timestamp",
@@ -19,7 +25,9 @@ const Table: React.FC<AlertsProps> = props => {
     },
     { Header: "Source", accessor: "source.type.name" },
     { Header: "Description", accessor: "description" },
-    { Header: "Details URL", accessor: "details_url" },
+    { Header: "Details URL", accessor: "details_url",
+      Cell: (row: { value: string; }) => (<a href={row.value}> {row.value} </a>)
+        },
     { Header: "Object", accessor: "object.name" },
     { Header: "Parent object", accessor: "parent_object.name" },
     { Header: "Problem type", accessor: "problem_type.name" }
