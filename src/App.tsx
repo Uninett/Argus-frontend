d6bdc2787ae8cd5b50a92585c9787af9dfa96008
Andreas Melzer <akrimelzer@gmail.com>
2019-11-13 08:53:54 +0100

    add component to view

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-
-import ProfileList from './components/profileList/ProfileList';
-
-import React from "react";
-import AlertView from "./views/alertView/AlertView";
-import LoginView from "./views/loginView/LoginView";
-import { Route, Switch } from "react-router-dom";
-import { ProtectedRoute } from "./protected.route";
-import "./variables.css";
-import "./colorscheme.css";
-import NotificationProfileView from "./views/notificationProfileView/NotificationProfileView";
-import ActiveProfile from "./components/active-profiles/ActiveProfile";
-import FilterBuildingView from "./views/filterBuildingView/filterBuildingView";
-
+import React from 'react';
+import AlertView from './views/alertView/AlertView';
+import LoginView from './views/loginView/LoginView';
+import { Route, Switch } from 'react-router-dom';
+import { ProtectedRoute } from './protected.route';
+import './variables.css';
+import './colorscheme.css';
+import NotificationProfileView from './views/notificationProfileView/NotificationProfileView';
+import FilterBuildingView from './views/filterBuildingView/filterBuildingView';
 
 const App: React.SFC = () => {
   return (
@@ -22,14 +17,9 @@ const App: React.SFC = () => {
           path='/notification-profile'
           component={NotificationProfileView}
         />
-
-        <Route path='/real-profile' component={ProfileList} />
-
-        <Route path="/login" component={LoginView} />
-        <Route path="/profile" component={ActiveProfile} />
-        <ProtectedRoute path="/customfilter" component={FilterBuildingView} />
-        <Route path="*" component={() => <h1>404 not found</h1>} />
-
+        <Route path='/login' component={LoginView} />
+        <ProtectedRoute path='/customfilter' component={FilterBuildingView} />
+        <Route path='*' component={() => <h1>404 not found</h1>} />
       </Switch>
     </div>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,40 +1,38 @@
-import React, { useState, useEffect } from "react";
-import "./Header.css";
-import auth from "../../auth";
-import aaslogo from "../../Media/img//logo/logo_white.svg";
-import { withRouter } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import auth from '../../auth';
+import aaslogo from '../../Media/img//logo/logo_white.svg';
+import { withRouter } from 'react-router-dom';
 
 const Userbutton: React.FC<{ history: any }> = props => {
-  const [user, setUser] = useState<any>("");
+  const [user, setUser] = useState<any>('');
 
   useEffect(() => {
     setTimeout(() => {
-      setUser(localStorage.getItem("user"));
+      setUser(localStorage.getItem('user'));
     }, 10);
   }, []);
 
   return (
-    <div id="header-user" className="headerbutton dropdown">
+    <div id='header-user' className='headerbutton dropdown'>
       <p>{user}</p>
-      <div className="dropdown-content">
+      <div className='dropdown-content'>
         <a
-          href="/notification-profile"
-          id="header-profiles"
-          className="headerbutton dropdown-button"
-        >
-          <p id="header-profiles" className="headerbutton dropdown-button">
+          href='/notification-profile'
+          id='header-profiles'
+          className='headerbutton dropdown-button'>
+          <p id='header-profiles' className='headerbutton dropdown-button'>
             Profiles
           </p>
         </a>
         <button
-          className="headerbutton dropdown-button"
-          id="header-logout"
+          className='headerbutton dropdown-button'
+          id='header-logout'
           onClick={() => {
             auth.logout(() => {
-              props.history.push("/login");
+              props.history.push('/login');
             });
-          }}
-        >
+          }}>
           Logout
         </button>
       </div>
@@ -44,18 +42,17 @@ const Userbutton: React.FC<{ history: any }> = props => {
 
 const Header: React.FC<{ history: any }> = props => {
   return (
-    <div className="header">
-      <a href="/">
-        <img src={aaslogo} alt="AAS logo" id="logo" />
+    <div className='header'>
+      <a href='/'>
+        <img src={aaslogo} alt='AAS logo' id='logo' />
       </a>
-      <a id="header-filters" className="headerbutton" href="/customfilter">
+      <a id='header-filters' className='headerbutton' href='/customfilter'>
         <p>Filters</p>
       </a>
       <a
-        id="header-notificationProfiles"
-        className="headerbutton"
-        href="/real-profile"
-      >
+        id='header-notificationProfiles'
+        className='headerbutton'
+        href='/notification-profile'>
         <p>Notification profiles</p>
       </a>
 
