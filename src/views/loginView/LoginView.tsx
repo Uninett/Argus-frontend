ea50cd8d31810661b786f3dc43d6a19b1046925e
Håvar Aambø Fosstveit <havar@fosstveit.net>
2020-06-08 09:39:18 +0200

    Fix path

--- src/views/loginView/LoginView.tsx
+++ src/views/loginView/LoginView.tsx
@@ -98,7 +98,7 @@ const LoginView: React.FC<any> = props => {
           </p>
           <a
             id='login-feide'
-            href='/login/dataporten_feide/'
+            href='/oidc//login/dataporten_feide/'
             target='_self'>
             Log in with Feide
           </a>
