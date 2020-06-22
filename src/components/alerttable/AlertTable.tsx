30e9b332638fe63ab1972be35991b0ec363509b8
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2020-06-22 17:05:33 +0200

    Reorder columns and add width constraints for alerts table
    For now the timestamp, source, problem type, object
    and parent object columns have a set max width given
    by either the elements currently in the table, or by
    a fixed size (the timestamp column).
    
    The description column width is not constrained at all
    but will use all the available space left for it.
    
    The columns are now in the following order:
    Timestamp,Source,Problem Type,Object,Parent Object,Description
    
    Reasoning:
    - Timestamp should be to the left because it is fixed size
      and always relevant
    - Source and Problem Type should probably be next to each
      other, but I'm not sure about their relative ordering.
    - Object and Parent Object should be next to each other,
      not sure about their relative ordering either.
    
    This technically closes #77

--- src/components/alerttable/AlertTable.tsx
+++ src/components/alerttable/AlertTable.tsx
@@ -3,16 +3,35 @@ import ReactTable from "react-table";
 import "./alerttable.css";
 import "react-table/react-table.css";
 
-import { AlertWithFormattedTimestamp } from "../../utils";
+import { AlertWithFormattedTimestamp, objectGetPropertyByPath } from "../../utils";
+
+// This is based on the following:
+// https://github.com/tannerlinsley/react-table/issues/94
+function calculateTableCellWidth(cellString: string, cssMagicSpacing: number = 9): number {
+  return cssMagicSpacing * cellString.length;
+}
+
+type Row = any;
+type ColumnAccessorFunction = (row: Row) => string;
+type Accessor = string | ColumnAccessorFunction;
+function getMaxColumnWidth(
+  rows: Row[],
+  headerText: string,
+  accessorValue: Accessor,
+  maxWidth: number = 600,
+  cssMagicSpacing: number = 11,
+): number {
+  const accessorFunction =
+    typeof accessorValue === "string" ? (row: Row) => objectGetPropertyByPath(row, accessorValue) : accessorValue;
+  const cellLength = (row: Row): number => (`${accessorFunction(row)}` || "").length;
+  const maxCellLength: number = rows.reduce((seen: number, row: Row) => Math.max(seen, cellLength(row)), 0);
+  return Math.min(maxWidth, cssMagicSpacing * Math.max(maxCellLength, headerText.length));
+}
 
 type AlertsProps = {
   alerts: AlertWithFormattedTimestamp[];
   noDataText: string;
 };
-
-//function formatDate(dateString: string):string {
-//  2014-09-15T11:25:42.833000+02:00
-//}
 
 const SourceDetailUrl = (row: { value: string; original: { details_url: string } }) => {
   return (
@@ -23,22 +42,60 @@ const SourceDetailUrl = (row: { value: string; original: { details_url: string }
   );
 };
 
+const centerStyle = { textAlign: "center" };
+
+type ConstrainedColumn = {
+  Header: string;
+  accessor: Accessor;
+  maxWidth?: number;
+  minWidth?: number;
+};
+
+type ConstraintFunction = (rows: Row[], header: string, accessor: Accessor) => number;
+function maxWidthColumn(rows: Row[], header: string, accessor: Accessor, func: ConstraintFunction): ConstrainedColumn {
+  return { Header: header, accessor, maxWidth: func(rows, header, accessor) };
+}
+
+function minWidthColumn(rows: Row[], header: string, accessor: Accessor, func: ConstraintFunction): ConstrainedColumn {
+  return { Header: header, accessor, minWidth: func(rows, header, accessor) };
+}
+
+function unboundedColumn(rows: Row[], header: string, accessor: Accessor, func: ConstraintFunction): ConstrainedColumn {
+  return { Header: header, accessor };
+}
+
 const AlertTable: React.FC<AlertsProps> = (props) => {
+  const rows: Row[] = props.alerts;
+
+  const timestampCellWidth: ConstraintFunction = (rows: Row[]) =>
+    calculateTableCellWidth("2015-11-14T03:04:14.387000+01:00");
+
   const columns: any = [
     {
-      Header: "Timestamp",
-      accessor: "timestamp",
       id: "timestamp_col",
+      ...maxWidthColumn(rows, "Timestamp", "timestamp", timestampCellWidth),
     },
-    { Header: "Problem type", accessor: "problem_type.name", id: "problem_type_col" },
-    { Header: "Description", accessor: "description", id: "description_col" },
-    { Header: "Object", accessor: "object.name", id: "object_col" },
-    { Header: "Parent object", accessor: "parent_object.name", id: "parent_object_col" },
     {
-      Header: "Source",
-      accessor: (row: any, i: any) => String(row.source.name),
       id: "source_col",
       Cell: SourceDetailUrl,
+      ...maxWidthColumn(rows, "Source", (row: Row) => String(row.source.name), getMaxColumnWidth),
+    },
+    {
+      id: "problem_type_col",
+      ...maxWidthColumn(rows, "Problem type", "problem_type.name", getMaxColumnWidth),
+    },
+    {
+      id: "object_col",
+      ...maxWidthColumn(rows, "Object", "object.name", getMaxColumnWidth),
+    },
+    {
+      id: "parent_object_col",
+      ...maxWidthColumn(rows, "Parent object", "parent_object.name", getMaxColumnWidth),
+    },
+    {
+      id: "description_col",
+      Header: "Description",
+      accessor: "description",
     },
   ];
 
