323fe976a12ba65265c9d377c6d753880dcaccd0
Håvar Aambø Fosstveit <havar@fosstveit.net>
2020-09-03 15:37:42 +0200

    Add settings to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,6 +17,9 @@ const Userbutton: React.FC<UserbuttonPropsType> = (props: UserbuttonPropsType) =
     <div id="header-user" className="headerbutton dropdown">
       <p>{props.user}</p>
       <div className="dropdown-content">
+        <Link id="header-settings" className="headerbutton" to="/settings">
+          <p>Settings</p>
+        </Link>
         <button
           className="headerbutton dropdown-button"
           id="header-logout"
