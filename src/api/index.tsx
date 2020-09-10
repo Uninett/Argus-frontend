ac3667cc786ba4c99ab9606a842c36467aec67d8
Hanne Moa <hanne.moa@uninett.no>
2020-09-10 14:57:05 +0200

    Make the first day of the week be number 1

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -38,17 +38,17 @@ export interface AuthTokenSuccessResponse {
   token: string;
 }
 
-export type TimeRecurrenceDay = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-export const TIME_RECURRENCE_DAY_IN_ORDER: TimeRecurrenceDay[] = [0, 1, 2, 3, 4, 5, 6];
+export type TimeRecurrenceDay = 1 | 2 | 3 | 4 | 5 | 6 | 7;
+export const TIME_RECURRENCE_DAY_IN_ORDER: TimeRecurrenceDay[] = [1, 2, 3, 4, 5, 6, 7];
 
 export const TimeRecurrenceDayNameMap: Record<TimeRecurrenceDay, string> = {
-  0: "Monday",
-  1: "Tuesday",
-  2: "Wednesday",
-  3: "Thursday",
-  4: "Friday",
-  5: "Saturday",
-  6: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+  7: "Sunday",
 };
 
 export interface TimeRecurrence {
