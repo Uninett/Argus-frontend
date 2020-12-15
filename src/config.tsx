c75fde658f8652302392dea5ba1e9f336741716d
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-12-15 14:16:59 +0100

    Make websocket support configurable through env

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "";
-export const ENABLE_WEBSOCKETS_SUPPORT = false;
+export const ENABLE_WEBSOCKETS_SUPPORT = process.env.REACT_APP_ENABLE_WEBSOCKETS_SUPPORT === "true" || false;
 export const BACKEND_WS_URL = process.env.REACT_APP_BACKEND_WS_URL || "";
 export const USE_SECURE_COOKIE = process.env.REACT_APP_USE_SECURE_COOKIE !== "false";
 export const DEBUG = process.env.REACT_APP_DEBUG === "true" || false;
