d8ab66b93c8fbaf780f819351e8477e82b26389a
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2020-12-17 13:00:47 +0100

    Add auto refresh functionality with configurable intervals

--- src/config.tsx
+++ src/config.tsx
@@ -3,3 +3,4 @@ export const ENABLE_WEBSOCKETS_SUPPORT = process.env.REACT_APP_ENABLE_WEBSOCKETS
 export const BACKEND_WS_URL = process.env.REACT_APP_BACKEND_WS_URL || "";
 export const USE_SECURE_COOKIE = process.env.REACT_APP_USE_SECURE_COOKIE !== "false";
 export const DEBUG = process.env.REACT_APP_DEBUG === "true" || false;
+export const DEFAULT_AUTO_REFRESH_INTERVAL = 30; // seconds
