9ba15d78900478dc71c5df12b6f944d5db5760ad
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2021-01-07 13:38:24 +0100

    CSS changes to make scroll-bar appear + starting on mobile support

--- src/views/login/LoginView.tsx
+++ src/views/login/LoginView.tsx
@@ -3,19 +3,20 @@ import React from "react";
 import "./LoginView.css";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
+import { useTheme } from "@material-ui/core/styles";
+import { useIsMobile, useBackground } from "../../hooks";
+
 import LoginForm from "../../components/login/Login";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      position: "fixed",
-      width: "100%",
-      height: "100%",
       margin: 0,
-      left: 0,
-      top: 0,
-      zIndex: 10,
-      background: `linear-gradient(45deg, ${theme.palette.primary.light} 30%, ${theme.palette.primary.dark} 90%)`,
+      height: "100vh",
+      width: "100vw",
+    },
+    mobileRoot: {
+      margin: 0,
     },
     formContainer: {
       display: "flex",
@@ -29,10 +30,16 @@ const useStyles = makeStyles((theme: Theme) =>
 type LoginViewPropsType = {};
 
 export const LoginView: React.FC<LoginViewPropsType> = () => {
+  const theme = useTheme();
   const style = useStyles();
+  useBackground(
+    `linear-gradient(45deg, ${theme.palette.primary.light} 30%, ${theme.palette.primary.dark} 90%) no-repeat fixed`,
+  );
+
+  const isMobile = useIsMobile();
 
   return (
-    <div className={style.root}>
+    <div className={isMobile ? style.mobileRoot : style.root}>
       <div className={style.formContainer}>
         <LoginForm />
       </div>
