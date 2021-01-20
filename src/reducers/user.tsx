493b0a5b7deee4dc341b85ba15b8e4ba13addbe6
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2021-01-20 12:42:57 +0100

    Add token login auth info to global state

--- src/reducers/user.tsx
+++ src/reducers/user.tsx
@@ -1,24 +1,35 @@
-import { User } from "../api";
+import { User, Token } from "../api";
 import { ActionMap, makeAction, makeActionWithoutPayload } from "./common";
 
 // For making it easier for components that need a default display name
 // even if the user isn't logged int, we add that to the state by default.
 // This also makes it so that the change will propogate automatically.
-export type UserStateType = { object: User | undefined; displayName: string };
+export type UserStateType = {
+  object: User | undefined;
+  displayName: string;
+  isAuthenticated: boolean;
+  isTokenAuthenticated: boolean;
+  token: string | undefined;
+};
 
 export enum UserType {
   Login = "LOGIN",
+  LoginToken = "LOGIN_TOKEN",
   Logout = "LOGOUT",
 }
 
 type UserPayload = {
   [UserType.Login]: User;
+  [UserType.LoginToken]: { user: User; token: Token };
   [UserType.Logout]: undefined;
 };
 
 export const initialUserState = {
   object: undefined,
   displayName: "Anonymous",
+  isAuthenticated: false,
+  isTokenAuthenticated: false,
+  token: undefined,
 };
 
 export type UserActions = ActionMap<UserPayload>[keyof ActionMap<UserPayload>];
@@ -28,6 +39,18 @@ export const userReducer = (state: UserStateType, action: UserActions) => {
       return {
         object: action.payload,
         displayName: action.payload.first_name,
+        isAuthenticated: true,
+        isTokenAuthenticated: false,
+        token: undefined,
+      };
+    case UserType.LoginToken:
+      const { user, token } = action.payload;
+      return {
+        token,
+        object: user,
+        displayName: user.first_name,
+        isAuthenticated: true,
+        isTokenAuthenticated: true,
       };
     case UserType.Logout: {
       return initialUserState;
@@ -38,4 +61,5 @@ export const userReducer = (state: UserStateType, action: UserActions) => {
 };
 
 export const loginUser = makeAction<UserType.Login, User>(UserType.Login);
+export const loginTokenUser = makeAction<UserType.LoginToken, { user: User; token: Token }>(UserType.LoginToken);
 export const logoutUser = makeActionWithoutPayload<UserType.Logout>(UserType.Logout);
