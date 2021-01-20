cef4b480cbc5d68c77b367689c8eae07b77c161a
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2021-01-20 12:13:48 +0100

    Extract `makeAction` from filter reducer to common

--- src/reducers/common.tsx
+++ src/reducers/common.tsx
@@ -8,3 +8,17 @@ export type ActionMap<M extends { [index: string]: unknown }> = {
         payload: M[Key];
       };
 };
+
+export type Action<T, P> = (payload: P) => { type: T; payload: P };
+export function makeAction<T, P>(type: T): Action<T, P> {
+  return (payload: P): { type: T; payload: P } => {
+    return { type, payload };
+  };
+}
+
+export type ActionWithoutPayload<T> = () => { type: T; payload: undefined };
+export function makeActionWithoutPayload<T>(type: T): ActionWithoutPayload<T> {
+  return (): { type: T; payload: undefined } => {
+    return { type, payload: undefined };
+  };
+}
