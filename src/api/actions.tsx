97ed69767d2eacc12b854d101f01a2129709973f
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2021-01-22 20:45:47 +0100

    Add `createFilter` action to useFilter() hook

--- src/api/actions.tsx
+++ src/api/actions.tsx
@@ -1,11 +1,28 @@
 import React, { useContext } from "react";
-import api, { Filter } from "../api";
+import api, { Filter, FilterSuccessResponse } from "../api";
 import { InitialStateType, ActionsType } from "../contexts";
-import { deleteFilter as deleteFilterAction, modifyFilter as modifyFilterAction } from "../reducers/filter";
+import {
+  createFilter as createFilterAction,
+  deleteFilter as deleteFilterAction,
+  modifyFilter as modifyFilterAction,
+} from "../reducers/filter";
 
 import { AppContext } from "../contexts";
 
 type Dispatch = React.Dispatch<ActionsType>;
+
+export const createFilter = (dispatch: Dispatch, filter: Omit<Filter, "pk">): Promise<Filter> => {
+  const definition = {
+    sourceSystemIds: filter.sourceSystemIds,
+    tags: filter.tags,
+  };
+  return api.postFilter(filter.name, definition).then((response: FilterSuccessResponse) => {
+    const { name, pk } = response;
+    const newFilter = { ...filter, name, pk };
+    dispatch(createFilterAction(newFilter));
+    return newFilter;
+  });
+};
 
 export const deleteFilter = (dispatch: Dispatch, pk: Filter["pk"]): Promise<void> => {
   return api.deleteFilter(pk).then(() => {
@@ -25,6 +42,7 @@ export const modifyFilter = (dispatch: Dispatch, filter: Filter): Promise<Filter
 };
 
 export type UseFiltersActionType = {
+  createFilter: (filter: Omit<Filter, "pk">) => ReturnType<typeof createFilter>;
   deleteFilter: (pk: Filter["pk"]) => ReturnType<typeof deleteFilter>;
   modifyFilter: (filter: Filter) => ReturnType<typeof modifyFilter>;
 };
@@ -38,6 +56,7 @@ export function useFilters(): [InitialStateType["filters"], UseFiltersActionType
   return [
     filters,
     {
+      createFilter: (filter: Omit<Filter, "pk">) => createFilter(dispatch, filter),
       deleteFilter: (pk: Filter["pk"]) => deleteFilter(dispatch, pk),
       modifyFilter: (filter: Filter) => modifyFilter(dispatch, filter),
     },
