51195db4e56b6e1c0194162b014fd00a7199a273
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2021-01-31 15:47:46 +0100

    Improve functionality of RealtimeService and RealtimeIncidentTable
    This patch just improves the previous functionality:
     1. It adds a "connected" state, that is set when the websocket
        connection is open, and we recieve the first response on
        our "subscribe" event.
    
     2. Instead of recreating the service instance just update the
        config by a new public method setConfig().
    
     3. Add a onStateChange callback to RealtimeService that allows
        for managing state in components based on the current realtime
        service state. This is now used to display alerts on changing
        state, and used to determine if it's loaded or not.
    
     4. Because there during a session will be created more than one
        realtime service instance, I've added an id that is assigned
        to every one (consecutive numbers), and is displayed in the
        log output. This makes it easier to catch bugs due to multiple
        active instances.

--- src/components/incidenttable/RealtimeIncidentTable.tsx
+++ src/components/incidenttable/RealtimeIncidentTable.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext, useMemo } from "react";
 
 // Api
 import { Incident, IncidentTag } from "../../api";
-import { RealtimeService } from "../../services/RealtimeService";
+import { RealtimeService, RealtimeServiceState, RealtimeServiceConfig } from "../../services/RealtimeService";
 
 // Contexts/Hooks
 import { useAlerts } from "../../components/alertsnackbar";
 import { useIncidents } from "../../api/actions";
-import { useIncidentsContext } from "../../components/incidentsprovider";
+import { useIncidentsContext, IncidentsContext } from "../../components/incidentsprovider";
 import { useSelectedFilter } from "../../components/filterprovider";
 
 // Utils
@@ -43,7 +43,9 @@ const matchesOnSources = (incident: Incident, sources: number[] | undefined): bo
 
 const matchesShow = (incident: Incident, show: "open" | "closed" | "both"): boolean => {
   if (show === "both") return true;
-  return incident.open ? show === "open" : show === "closed";
+  const openState = show == "open";
+  return incident.open === openState;
+  // return incident.open ? show === "open" : show === "closed";
 };
 
 const matchesAcked = (incident: Incident, showAcked: boolean): boolean => {
@@ -62,11 +64,54 @@ const RealtimeIncidentTable = () => {
     },
     {},
   ] = useSelectedFilter();
-  const [{ incidents }, { loadAllIncidents, addIncident, modifyIncident, removeIncident }] = useIncidentsContext();
+  const [{}, { addIncident, modifyIncident, removeIncident }] = useIncidentsContext();
   const [{}, { loadIncidentsFiltered }] = useIncidents();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isLoadingRealtime, setIsLoadingRealtime] = useState<boolean>(true);
-  const [filterMatcher, setFilterMatcher] = useState<(incident: Incident) => boolean>(() => false);
+  const [filterMatcher, setFilterMatcher] = useState<((incident: Incident) => boolean) | undefined>(undefined);
+
+  const [rtsState, setRtsState] = useState<RealtimeServiceState | undefined>(undefined);
+  const [rtsConfig, setRtsConfig] = useState<RealtimeServiceConfig | undefined>(undefined);
+  const [realtimeService, setRealtimeService] = useState<RealtimeService | undefined>(undefined);
+
+  // const onIncidentsInitial = (/* incidents: Incident[] */) => {
+  //   // const matchesFilter = incidents.filter((incident: Incident) =>
+  //   //   incidentMatchesFilter(incident, { showAcked, show, tags, sourcesById, autoUpdate }),
+  //   // );
+  //   // loadAllIncidents(matchesFilter);
+  //   // setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    console.log("rtsState", rtsState);
+    switch (rtsState) {
+      case "opened":
+        // displayAlert("Realtime opened", "info");
+        setIsLoadingRealtime(true);
+        break;
+
+      case "connected":
+        displayAlert("Realtime connected", "success");
+        setIsLoadingRealtime(false);
+        break;
+
+      case "closed": {
+        setIsLoadingRealtime(true);
+        displayAlert("Realtime closed", "warning");
+        break;
+      }
+
+      case "disconnecting":
+        setIsLoadingRealtime(true);
+        displayAlert("Realtime disconnecting", "info");
+        break;
+
+      case "connecting":
+        displayAlert("Realtime connecting", "info");
+        setIsLoadingRealtime(true);
+        break;
+    }
+  }, [rtsState, displayAlert]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -81,19 +126,40 @@ const RealtimeIncidentTable = () => {
     // function we need to create a new such function every time the filter changes.
     const incidentMatchesFilter = (incident: Incident): boolean => {
       const filter = { showAcked, show, tags, sourcesById, autoUpdate };
-      const matches =
+
+      /*
+      // Useful for debugging
+      const matches = [
+        ["open", incident.open, filter.show, matchesShow(incident, filter.show)],
+        ["acked", incident.acked, filter.showAcked, matchesAcked(incident, filter.showAcked)],
+        ["tags", incident.tags, filter.tags, matchesOnTags(incident, filter.tags)],
+        ["source", incident.source, filter.sourcesById, matchesOnSources(incident, filter.sourcesById)],
+      ];
+
+      console.debug("Checking match on filter: ");
+      matches.forEach(([name, incidentValue, filterValue, match]) => {
+        console.debug(match ? "match" : "fail", `${name}: `, incidentValue, filterValue);
+      });
+
+      return matches.every(([name, incidentValue, filterValue, match]) => match);
+       */
+      return (
         matchesShow(incident, filter.show) &&
         matchesAcked(incident, filter.showAcked) &&
         matchesOnTags(incident, filter.tags) &&
-        matchesOnSources(incident, filter.sourcesById);
-      return matches;
+        matchesOnSources(incident, filter.sourcesById)
+      );
     };
     setFilterMatcher(() => incidentMatchesFilter);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showAcked, show, tags, sourcesById, autoUpdate]);
 
+  // Effect for creating RealtimeServiceConfig
   useEffect(() => {
-    setIsLoadingRealtime(true);
+    if (filterMatcher === undefined) {
+      // Wait until it is set.
+      return;
+    }
 
     const onIncidentAdded = (incident: Incident) => {
       if (filterMatcher(incident)) {
@@ -113,36 +179,94 @@ const RealtimeIncidentTable = () => {
       removeIncident(incident.pk);
     };
 
-    const onIncidentsInitial = (/* incidents: Incident[] */) => {
-      // const matchesFilter = incidents.filter((incident: Incident) =>
-      //   incidentMatchesFilter(incident, { showAcked, show, tags, sourcesById, autoUpdate }),
-      // );
-      // loadAllIncidents(matchesFilter);
-      // setIsLoading(false);
-      setIsLoadingRealtime(false);
-    };
-
-    const rts = new RealtimeService({
+    const config: RealtimeServiceConfig = {
       onIncidentAdded,
       onIncidentModified,
       onIncidentRemoved,
-      onIncidentsInitial,
-    });
+      onIncidentsInitial: () => undefined,
+    };
+    setRtsConfig(config);
+  }, [filterMatcher, addIncident, modifyIncident, removeIncident]);
+
+  // Update realtime service instance config
+  useEffect(() => {
+    if (rtsConfig === undefined || realtimeService === undefined) {
+      return;
+    }
+
+    if (realtimeService.config === rtsConfig) {
+      return;
+    }
+    console.log("updating config", rtsConfig);
+    realtimeService.setConfig(rtsConfig);
+
+    realtimeService.onStateChange = (prev: RealtimeServiceState, curr: RealtimeServiceState) => {
+      console.log("rts.onStateChange", prev, curr);
+
+      if (curr === prev) {
+        // Ignore.
+        return;
+      }
+
+      switch (curr) {
+        case "opened":
+          displayAlert("Realtime opened", "info");
+          setIsLoadingRealtime(true);
+          break;
+
+        case "connected":
+          displayAlert("Realtime connected", "success");
+          setIsLoadingRealtime(false);
+          break;
+
+        case "closed": {
+          setIsLoadingRealtime(true);
+          displayAlert("Realtime closed", "warning");
+          break;
+        }
+
+        case "disconnecting":
+          setIsLoadingRealtime(true);
+          displayAlert("Realtime disconnecting", "info");
+          break;
+
+        case "connecting":
+          displayAlert("Realtime connecting", "info");
+          setIsLoadingRealtime(true);
+          break;
+      }
+      setRtsState(curr);
+      return;
+    };
+  }, [rtsConfig, realtimeService, displayAlert]);
+
+  useEffect(() => {
+    if (rtsConfig === undefined) {
+      // Wait until rtsConfig is set
+      return;
+    }
+
+    if (realtimeService !== undefined) {
+      // We have already created an realtime service,
+      // so don't make another one.
+      return;
+    }
+
+    console.log("Creating realtime service!");
+
+    setIsLoadingRealtime(true);
+
+    const rts = new RealtimeService(rtsConfig);
+    setRealtimeService(rts);
     rts.connect();
     console.debug("created rts and connect()");
-    displayAlert("Realtime enabled", "success");
 
     return () => {
       console.debug("disconnecting rts");
-      displayAlert("Realtime disconnected", "warning");
       rts.disconnect();
-      setIsLoadingRealtime(true);
+      rts.resetOnStateChange();
     };
-    // When the websockets backend implementation support taking filters
-    // this might be useful:
-    //}, [showAcked, show, tags, sourcesById, autoUpdate]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [rtsConfig, realtimeService, displayAlert]);
 
   return <MinimalIncidentTable isRealtime isLoading={isLoading} isLoadingRealtime={isLoadingRealtime} />;
 };
