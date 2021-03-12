2eb785a735321a3e3578a556e7b13d03d3cd98ab
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2021-03-12 11:11:45 +0100

    Add missing localstorageconsts file

---
+++ src/localstorageconsts.ts
@@ -0,0 +1,6 @@
+// These constants just defined the keys in localstorage
+// Placed here because they are useful to store as constants,
+// but shouldn't ever require user changes
+export const SELECTED_FILTER = "selectedfilter";
+export const PAGINATION_CURSOR_PAGE_SIZE = "paginationcursor";
+export const DROPDOWN_TOOLBAR = "dropdowntoolbar";
