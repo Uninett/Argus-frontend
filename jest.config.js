22ebd62b0ac71b35151ca16b05f2d75d1a24402e
Ilona Podliashanyk <ilonap@stud.ntnu.no>
2021-06-16 09:23:46 +0200

    Add basic jest.config file.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  setupFilesAfterEnv: [
+    "@testing-library/react/cleanup-after-each",
+    "@testing-library/jest-dom/extend-expect",
+  ],
+  testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  verbose: true
+}
