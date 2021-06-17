97fd0ba80c9f711b1f868a1fb21d40e3926944e4
Herman Martinsen <martinsenhr@gmail.com>
2021-06-17 08:33:32 +0200

    Fixing dependencies for React Testing Library and installing Axios Mock Adapter.

--- src/components/login/Login.test.tsx
+++ src/components/login/Login.test.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import MockAdapter from "axios-mock-adapter";
 
 import LoginForm from "./Login";
+import api from "../../api";
+
+const mock = new MockAdapter(api.api);
 
 describe("Render login page", () => {
   it("renders Logo", () => {
@@ -25,5 +29,19 @@ describe("Render login page", () => {
   it("renders Link to Feide", () => {
     render(<LoginForm />);
     expect(screen.getByRole("link")).toBeInTheDocument();
+  });
+});
+
+describe("Unsuccessful login", () => {
+  it("renders Logo", async () => {
+    mock.onPost("/api/v1/token-auth/").reply(400);
+
+    render(<LoginForm />);
+
+    await userEvent.click(screen.getByRole("button"));
+
+    const message = await screen.findByText(/Wrong username or password/);
+
+    screen.debug();
   });
 });
