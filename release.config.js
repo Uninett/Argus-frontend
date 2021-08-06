60630ba1c6a85ebe221979bf471301fbfc5e9563
Ilona Podliashanyk <ilonap@ntnu.no>
2021-08-06 08:33:55 +0200

    Add sem-ver git dependency
    Git library in sem-ver git lets automatically update version in package.json after the successful release. Without it package.sjon would have to be updated manually after each release.

--- release.config.js
+++ release.config.js
@@ -4,6 +4,10 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
+        ["@semantic-release/npm", {
+            "npmPublish": false,
+        }],
+        "@semantic-release/git",
         "@semantic-release/github"
     ]
 }
