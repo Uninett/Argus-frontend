0f4550dce40b7729e029f97b5765a4548e6fe457
Ilona Podliashanyk <ilonap@ntnu.no>
2021-08-16 12:25:55 +0200

    Fix failing workflow

--- src/components/dialogs/ConfirmDialog.tsx
+++ src/components/dialogs/ConfirmDialog.tsx
@@ -5,8 +5,6 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
 
 export type ConfirmDialogPropsType = {
   title: string;
