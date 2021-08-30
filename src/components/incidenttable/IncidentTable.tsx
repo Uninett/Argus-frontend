6334111d0a8aeefef7f6e29091c650bfcecd2323
Ilona Podliashanyk <ilonap@ntnu.no>
2021-08-30 13:21:24 +0200

    Remove unused imports
    Fixes failed workflow

--- src/components/incidenttable/IncidentTable.tsx
+++ src/components/incidenttable/IncidentTable.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo, useCallback, SetStateAction, Dispatch, useEffect} from "react";
+import React, {useState, useMemo, useCallback, useEffect} from "react";
 
 import { Link } from "react-router-dom";
 
