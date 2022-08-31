c145b8dc054079e3f1abd9605fe243938cabb4fb
Ilona Podliashanyk <ilonap@ntnu.no>
2022-08-31 13:22:02 +0200

    Close realtime in global state on error

--- src/services/RealtimeService.ts
+++ src/services/RealtimeService.ts
@@ -212,7 +212,7 @@ export class RealtimeService {
     this.ws.onerror = (e: any) => {
       console.error(`[RealtimeService ${this.id}] got error on websocket client, closing: ${e.message}`);
       if (this.ws) this.ws.close();
-      // this.setState("closed"); // TODO: should this be set?
+      this.setState("closed"); 
     };
   }
 }
