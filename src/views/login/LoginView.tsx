2bc9526e6c38f493b4a3a5a44ebcf95e8a06615a
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2023-03-21 13:34:35 +0100

    Add AlertAppbar to login view on server errors

--- src/views/login/LoginView.tsx
+++ src/views/login/LoginView.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 import "./LoginView.css";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import {createStyles, makeStyles } from "@material-ui/core/styles";
 
 import { useTheme } from "@material-ui/core/styles";
 import { useIsMobile, useBackground } from "../../hooks";
 
 import LoginForm from "../../components/login/Login";
+import {useApiState} from "../../state/hooks";
+import AlertAppbar from "../../components/alerts/AlertAppbar";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -39,12 +41,19 @@ export const LoginView: React.FC<LoginViewPropsType> = () => {
 
   const isMobile = useIsMobile();
 
+  const [apiState] = useApiState();
+
   return (
     <div className={isMobile ? style.mobileRoot : style.root}>
       <Helmet>
         <title>Argus | Login</title>
       </Helmet>
       <div className={style.formContainer}>
+        {apiState.hasConnectionProblems && (
+          <AlertAppbar
+            message={"Problems connecting to server... Check your connection."}
+            severity={"error"}/>
+        )}
         <LoginForm />
       </div>
     </div>
