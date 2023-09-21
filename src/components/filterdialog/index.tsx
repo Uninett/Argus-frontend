1ca2fd21edea530581fe12df2a05fe95ec56b8ee
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2023-09-21 16:02:18 +0200

    Catch errors on filter deletion

--- src/components/filterdialog/index.tsx
+++ src/components/filterdialog/index.tsx
@@ -57,9 +57,13 @@ export const FilterDialog = ({ open, onClose }: FilterDialogPropsType) => {
   const displayAlert = useAlerts();
 
   const onDelete = (filter: Filter) => {
-    deleteFilter(filter.pk).then(() => {
-      displayAlert(`Deleted filter ${filter.name}`, "warning");
-    });
+    deleteFilter(filter.pk)
+      .then(() => {
+        displayAlert(`Deleted filter ${filter.name}`, "warning");
+      })
+      .catch((error) => {
+        displayAlert(`${error.message}`, "error")
+      });
   };
 
   return (
